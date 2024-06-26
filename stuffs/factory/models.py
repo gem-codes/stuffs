214e693567bc8712031b501d2e8b914c6b3dd84f
gem-codes <matef.gemm@gmail.com>
2024-06-26 12:23:52 -0700

    Created is_fully_assigned and mark_completed function

--- stuffs/factory/models.py
+++ stuffs/factory/models.py
@@ -6,8 +6,15 @@ class Specification(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   is_completed = models.BooleanField(default=False)
 
+  def is_fully_assigned(self):
+    return all(component.part_code for group in self.groups.all() for component in group.components.all())
+
+  def check_and_mark_completed(self):
+    self.is_completed = self.is_fully_assigned()
+    self.save()
+   
   def __str__(self):
-    return f"#{self.name}"
+    return f"{self.id} {self.name}"
 
 class Group(models.Model):
   specification = models.ForeignKey(
@@ -19,7 +26,7 @@ class Group(models.Model):
   group_code = models.CharField(max_length=20)
 
   def __str__(self):
-    return f"#{self.name}"
+    return f"{self.id} {self.name}"
   
 class Component(models.Model):
   group = models.ForeignKey(
@@ -32,4 +39,4 @@ class Component(models.Model):
   part_code = models.CharField(max_length=20, blank=True, null=True)
 
   def __str__(self):
-    return self.name
+    return f"{self.id} {self.name}"
